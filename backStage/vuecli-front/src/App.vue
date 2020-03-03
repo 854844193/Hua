930ae300b3d4384c340c3056f3e50ab96a182ea0
854844193 <854844193@qq.com>
2020-03-03 14:33:03 +0800

    前端和后台管理的环境文件

---
+++ backStage/vuecli-front/src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="app">
+    <el-menu
+      :default-active="activeIndex2"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+    >
+      <el-menu-item index="1"><router-link to="/">首页</router-link></el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">我的工作台</template>
+        <el-menu-item index="2-1">选项1</el-menu-item>
+        <el-menu-item index="2-2">选项2</el-menu-item>
+        <el-menu-item index="2-3">选项3</el-menu-item>
+        <el-menu-item index="2-4">选项4</el-menu-item>
+        <!-- <el-submenu index="2-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="2-4-1">选项1</el-menu-item>
+          <el-menu-item index="2-4-2">选项2</el-menu-item>
+          <el-menu-item index="2-4-3">选项3</el-menu-item>
+        </el-submenu> -->
+      </el-submenu>
+      <!-- <el-menu-item index="3" disabled><router-link to="/about">详情页</router-link></el-menu-item> -->
+      <el-menu-item index="3"><router-link to="/about">详情页</router-link></el-menu-item>
+      <el-menu-item index="4">
+        <a href="https://www.ele.me" target="_blank">订单管理</a>
+      </el-menu-item>
+      <el-menu-item index="5" ><router-link to="/Uinfo">个人信息</router-link></el-menu-item>
+    </el-menu>
+    <router-view />
+  </div>
+</template>
+
+
+<script>
+  export default {
+    data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1'
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
+<style>
+*{
+  padding: 0;
+  margin: 0;
+}
+</style>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
